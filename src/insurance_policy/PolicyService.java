fbd45bffddcf525bb4d2e122b4c9a44096fcb3e1
Gaurav-303 <gaurav134mishra@gmail.com>
2025-09-01 00:09:08 +0530

    add policy

---
+++ src/insurance_policy/PolicyService.java
@@ -0,0 +1,17 @@
+package insurance_policy;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class PolicyService {
+
+    private Map<String, Policy> hashMapPolicies = new HashMap<>();
+    private Map<String, Policy> linkedHashMapPolicies = new LinkedHashMap<>();
+    private TreeMap<LocalDate, Policy> treeMapPolicies = new TreeMap<>();
+
+    public void addPolicy(Policy policy) {
+        hashMapPolicies.put(policy.getPolicyNumber(), policy);
+        linkedHashMapPolicies.put(policy.getPolicyNumber(), policy);
+        treeMapPolicies.put(policy.getExpiryDate(), policy);
+    }
+}
